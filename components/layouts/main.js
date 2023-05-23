19c5fd3aeccf7a35e1c9156a78aa9fae4f48aa0b
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-05-23 13:14:42 -0300

    feature: add 3D dog glb using three js lib

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
+import NoSsr from '../no-ssr.js'
+import VoxelDog from '../voxel.js'
 
 const Main = ({ children, router }) => {
   return (
@@ -12,6 +14,9 @@ const Main = ({ children, router }) => {
 
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <VoxelDog />
+        </NoSsr>
         {children}
       </Container>
     </Box>
