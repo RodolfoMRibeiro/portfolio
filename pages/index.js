0aa68462bf5260a80e0ba93402cccbb390874c4a
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-05-22 23:57:59 -0300

    feature: add animated Section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        mb={6}
+        p={3}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Brazil!
       </Box>
 
@@ -14,7 +27,31 @@ const Page = () => {
           </Heading>
           <p>Digital CyberPunk (Developer / Cloud Associate / Reader)</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="images/rodolfo.jpg"
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
