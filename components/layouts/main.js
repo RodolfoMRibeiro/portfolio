763260258e4458ee498b3776907f85818888a257
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-05-28 21:22:55 -0300

    feature: add favicon image

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,7 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/images/favicon.png"></link>
         <title>Rodolfo Marques - Homepage</title>
       </Head>
 
