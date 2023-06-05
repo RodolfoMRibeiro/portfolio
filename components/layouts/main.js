907c0362ad38c35fb08cb191c07863b9b8b91899
rodolfo.ribeiro_q2pa <rodolfo.ribeiro@q2pay.com.br>
2023-06-05 10:35:09 -0300

    feature: add metatag

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,14 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="I am a Software Engineer skilled in full cycle development, cloud computing, and DevOps. Browse through my portfolio to see my projects and expertise."
+        />
+        <meta
+          name="keywords"
+          content="software engineer, full cycle development, cloud associate, DevOps, portfolio"
+        />
         <link rel="icon" type="image/x-icon" href="/images/favicon.png"></link>
         <title>Rodolfo Marques - Homepage</title>
       </Head>
